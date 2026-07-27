3eae42d96ff6a61d4e692d6836fd013d81afa9b9
Florina Simeria <florinalatcu@yahoo.com>
2026-07-27 17:26:23 +0300

    Add login test for successful user authentication

---
+++ tests/login.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+import { users } from '../fixtures/testData';
+
+test('Successful login', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.navigate();
+  await loginPage.login(
+    users.standard.username,
+    users.standard.password
+  );
+
+  await expect(page).toHaveURL(/inventory/);
+});
